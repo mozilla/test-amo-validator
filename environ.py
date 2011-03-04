96622cde47201f75176f40b3d5350c44eaa8dd9c
Kumar McMillan <kumar.mcmillan@gmail.com>
2011-03-04 15:21:07 -0600

    Better relative path handling

--- environ.py
+++ environ.py
@@ -8,19 +8,23 @@ class conf:
     done = False
 
 
+def rel(path):
+    return os.path.join(os.path.dirname(__file__), path)
+
+
 def setup():
     if conf.done:
         return
-    site.addsitedir('./zamboni/apps')
-    site.addsitedir('./zamboni-lib/lib')
-    site.addsitedir('./zamboni')
-    site.addsitedir('./zamboni-lib/lib')
+    site.addsitedir(rel('zamboni/apps'))
+    site.addsitedir(rel('zamboni-lib/lib'))
+    site.addsitedir(rel('zamboni'))
+    site.addsitedir(rel('zamboni-lib/lib'))
 
-    sys.path.append('./zamboni/lib')
-    sys.path.append('./zamboni-lib/lib/python')
+    sys.path.append(rel('zamboni/lib'))
+    sys.path.append(rel('zamboni-lib/lib/python'))
 
-    for spec in ['./zamboni/apps/*',
-                 './zamboni-lib/src/*']:
+    for spec in [rel('zamboni/apps/*'),
+                 rel('zamboni-lib/src/*')]:
         for d in glob.glob(spec):
             sys.path.append(d)
 
