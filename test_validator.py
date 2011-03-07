e86cf49a2b6cca0f81f7c2b3a83a1c2a8fea395b
Kumar McMillan <kumar.mcmillan@gmail.com>
2011-03-07 15:32:19 -0600

    Adds success expectations, security tests, and search tests

--- test_validator.py
+++ test_validator.py
@@ -24,6 +24,9 @@ def _validator(file_path):
                     spidermonkey=js)
 
 
+_cached_validation = {}
+
+
 class ValidatorTest(unittest.TestCase):
 
     def msg_set(self, d):
@@ -31,7 +34,18 @@ class ValidatorTest(unittest.TestCase):
 
     def validate(self, xpi):
         path = os.path.join(os.path.dirname(__file__), 'addons', xpi)
-        return json.loads(_validator(path))
+        if path in _cached_validation:
+            return _cached_validation[path]
+        v = json.loads(_validator(path))
+        _cached_validation[path] = v
+        return v
+
+    def assertPartialMsg(self, msgs, partial_msg):
+        found = False
+        for m in msgs:
+            if m.startswith(partial_msg):
+                found = True
+        assert found, ('Unexpected: %r' % msgs)
 
 
 class JavaScriptTests(ValidatorTest):
@@ -44,11 +58,7 @@ class JavaScriptTests(ValidatorTest):
         """
         d = self.validate('glee-20101227219.xpi')
         msg = self.msg_set(d)
-        found = False
-        for m in msg:
-            if m.startswith('createElement() used to create script tag'):
-                found = True
-        assert found, ('Unexpected: %r' % msg)
+        self.assertPartialMsg(msg, 'createElement() used to create script tag')
 
     def test_dangerous_global(self):
         """Dangerous Global Object"""
@@ -165,3 +175,115 @@ class GeneralTests(ValidatorTest):
         d = self.validate('littlemonkey-1.8.56-sm.xpi')
         msg = self.msg_set(d)
         assert u'Addon missing install.rdf.' in msg, ('Unexpected: %r' % msg)
+
+    def test_invalid_id(self):
+        """invalid id"""
+        d = self.validate('add-ongoogle-201101121132.xpi')
+        msg = self.msg_set(d)
+        assert u'The value of <em:id> is invalid.' in msg, (
+                                                'Unexpected: %r' % msg)
+
+    def test_xpi_cannot(self):
+        """XPI cannot be opened"""
+        d = self.validate('lavafox_test-theme-20101130538.xpi')
+        msg = self.msg_set(d)
+        assert u'The XPI could not be opened.' in msg, (
+                                                'Unexpected: %r' % msg)
+
+    def test_invalid_version(self):
+        d = self.validate('invalid maximum version number.xpi')
+        msg = self.msg_set(d)
+        assert u'Invalid maximum version number' in msg, (
+                                                'Unexpected: %r' % msg)
+
+
+class LocalizationTests(ValidatorTest):
+
+    def test_translation(self):
+        d = self.validate('babuji-20110124355.xpi')
+        msg = self.msg_set(d)
+        assert u'Unchanged translation entities' in msg, (
+                                                'Unexpected: %r' % msg)
+
+    def test_encodings(self):
+        d = self.validate('babuji-20110124355.xpi')
+        msg = self.msg_set(d)
+        assert u'Unexpected encodings in L10n files' in msg, (
+                                                'Unexpected: %r' % msg)
+
+    def test_missing_translation(self):
+        """Missing Translation files"""
+        d = self.validate('download_statusbar-0.9.7.2-fx (1).xpi')
+        msg = self.msg_set(d)
+        assert u'Missing translation entity' in msg, (
+                                                'Unexpected: %r' % msg)
+
+
+class SecurityTests(ValidatorTest):
+
+    def test_missing_comments(self):
+        """Missing comments in <script> tag"""
+        d = self.validate('add-on-20110113408.xpi')
+        msg = self.msg_set(d)
+        assert u'Missing comments in <script> tag' in msg, (
+                                                'Unexpected: %r' % msg)
+
+    def test_typeless_iframes_browsers(self):
+        """Typeless iframes:browsers must be local"""
+        d = self.validate('add-on201101081038.xpi')
+        msg = self.msg_set(d)
+        assert u'Typeless iframes/browsers must be local.' in msg, (
+                                                'Unexpected: %r' % msg)
+
+
+class NoErrorsExpected(ValidatorTest):
+
+    def test_an_attempt(self):
+        """An attempt to overwrite a global varible [sic] made in some JS code"""
+        d = self.validate('tmp.xpi')
+        eq_(d['errors'], 0)
+
+    def test_don_t_freak(self):
+        """don't freak on UTF-8 chs in JS files """
+        d = self.validate('test (1).xpi')
+        eq_(d['errors'], 0)
+
+    def test_don_t_freak_2(self):
+        """don't freak out if install.js is missing"""
+        d = self.validate('littlemonkey-1.8.56-sm.xpi')
+        msg = self.msg_set(d)
+        ok = True
+        for m in msg:
+            if 'install.js' in msg:
+                ok = False
+        assert ok, ('Unexpected: %r' % msg)
+
+    def test_unknown_file(self):
+        """Unknown file found in add-on"""
+        d = self.validate('add-on20101228444 (1).jar')
+        eq_(d['errors'], 0)
+
+
+class SearchTools(ValidatorTest):
+
+    def test_opensearch_providers(self):
+        """OpenSearch providers cannot contain <Url :> ..."""
+        d = self.validate('sms_search-20110115 .xml')
+        msg = self.msg_set(d)
+        self.assertPartialMsg(msg,
+            'OpenSearch: Per AMO guidelines, OpenSearch providers '
+            'cannot contain <Url />')
+
+    def test_opensearch_shortname(self):
+        """OpenSearch ShortName values longer than 16 characters"""
+        d = self.validate('lexisone_citation_search-20100116 .xml')
+        msg = self.msg_set(d)
+        assert u'OpenSearch: <ShortName> too long; must be <17 characters' in msg, (
+                                                'Unexpected: %r' % msg)
+
+    def test_too_many(self):
+        """Too many <ShortName> elements"""
+        d = self.validate('addon-12201-latest.xml')
+        msg = self.msg_set(d)
+        assert u'OpenSearch: Too many <ShortName> elements' in msg, (
+                                                'Unexpected: %r' % msg)
